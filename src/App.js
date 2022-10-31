4145688facfc0db4ea10f11d4dbf995b717368bc
singh-maya <singh.maya@ufl.edu>
2022-10-31 19:32:38 -0400

    added log in and registration

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import {BrowserRouter as BrowserRouter, Route, Routes} from 'react-router-dom';
 import Notifications from './components/Notification';
-import Settings from './components/Settings';
-import Home from './components/Home';
+import Settings from './Pages/Settings';
+import Home from './Pages/Home';
 import Navigationbar from './components/Navigationbar';
-import Profile from './components/Profile';
+import Login from './Pages/Login'
+import { AuthProvider } from "./context/AuthContext";
+import Profile from './Pages/Profile'
+import Register from "./Pages/Regsitration";
+
 
 function App() {
   return (
     <div className="App">
         <BrowserRouter>
-            <Navigationbar />
-            <Routes>
-                <Route exact path='/' element={<Home/>}/>
-                <Route path='/Notifications' element={<Notifications/>}/>
-                <Route path='/Settings' element={<Settings/>}/>
-                <Route path='/Profile' element={<Profile/>}/>
-            </Routes>
+            <AuthProvider>
+                <Navigationbar />
+                <Routes>
+                    <Route exact path='/' element={<Home/>}/>
+                    <Route path='/Notifications' element={<Notifications/>}/>
+                    <Route path='/Settings' element={<Settings/>}/>
+                    <Route path='/Profile' element={<Profile/>}/>
+                    <Route path = "/Login" element={<Login/>}/>
+                     <Route path = "/Register" element={<Register/>}/>
+                </Routes>
+            </AuthProvider>
         </BrowserRouter>
     </div>
   );
--- src/components/Navigationbar.js
+++ src/components/Navigationbar.js
@@ -2,57 +2,66 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { Nav, Navbar, NavLink, NavItem } from "react-bootstrap";
 import { Link } from "react-router-dom";
-
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Tooltip from 'react-bootstrap/Tooltip';
+import {useContext, useEffect, useState} from "react";
 
 import{HouseDoor, Bell, Gear, SearchHeart, CircleFill} from "react-bootstrap-icons";
 
-function Trigger(){
-    const renderTooltip = (props) => (
-        <Tooltip id="button-tooltip" {...props}>
-            Simple tooltip
-        </Tooltip>
-    )
-}
+import AuthContext from "../context/AuthContext";
+import Login from "../Pages/Login";
+import Registration from "../Pages/Regsitration";
+
+
 const Navigationbar = () => {
+    const { user} = useContext(AuthContext);
     return (
-        <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
-            <Navbar.Toggle aria-controls="navbarScroll" data-bs-toggle="collapse" data-bs-target="#navbarScroll" />
-            <Navbar.Collapse id="navbarScroll">
-                <Form className="d-flex">
-                    <Form.Control
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                    />
-                    <Button variant = "dark" ><SearchHeart /></Button>
-            </Form>
-                <Nav className = "ms-auto">
-                    <Button size = "lg" variant = "none">
-                        <NavLink  eventKey="1" as={Link} to="/">
-                            <HouseDoor />
-                        </NavLink>
-                    </Button>
-                    <Button size = "lg" variant = "none">
-                        <NavLink  eventKey="2" as={Link} to="/notifications">
-                            <Bell />
-                        </NavLink>
-                    </Button>
-                    <Button size = "lg" variant = "none">
-                        <NavLink  eventKey="3" as={Link} to="/settings">
-                            <Gear />
-                        </NavLink>
-                    </Button>
-                    <Button size = "lg" variant = "none">
-                        <NavLink  eventKey="4" as={Link} to="/profile">
-                            <CircleFill />
-                        </NavLink>
-                    </Button>
-                </Nav>
-            </Navbar.Collapse>     
-        </Navbar>
+        <div>
+        {user ? (
+            <>
+                <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
+                        <Navbar.Toggle aria-controls="navbarScroll" data-bs-toggle="collapse" data-bs-target="#navbarScroll" />
+                        <Navbar.Collapse id="navbarScroll">
+                            <Form className="d-flex">
+                                <Form.Control
+                                type="search"
+                                placeholder="Search"
+                                className="me-2"
+                                aria-label="Search"
+                                />
+                                <Button variant = "dark" ><SearchHeart /></Button>
+                        </Form>
+                            <Nav className = "ms-auto">
+                                <Button size = "lg" variant = "none">
+                                    <NavLink  eventKey="1" as={Link} to="/">
+                                        <HouseDoor />
+                                    </NavLink>
+                                </Button>
+                                <Button size = "lg" variant = "none">
+                                    <NavLink  eventKey="2" as={Link} to="/notifications">
+                                        <Bell />
+                                    </NavLink>
+                                </Button>
+                                <Button size = "lg" variant = "none">
+                                    <NavLink  eventKey="3" as={Link} to="/settings">
+                                        <Gear />
+                                    </NavLink>
+                                </Button>
+                                <Button size = "lg" variant = "none">
+                                    <NavLink  eventKey="4" as={Link} to="/profile">
+                                        <CircleFill />
+                                    </NavLink>
+                                </Button>
+                            </Nav>
+                        </Navbar.Collapse>
+                </Navbar>
+            </>
+        ):(
+            <>
+                <Link to="/login">Login</Link>
+                <Link to="/register">Register</Link>
+            </>
+
+        )}
+        </div>
     );
 }
  

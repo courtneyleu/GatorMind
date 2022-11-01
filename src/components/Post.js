6ac21a400eac8307d330a58f4e7966115f184c6b
Meryem Yuksel <myuksel@ufl.edu>
2022-11-01 10:48:16 -0400

    Post file format created

---
+++ src/components/Post.js
@@ -0,0 +1,16 @@
+const Post = () =>{
+return(
+    <div>
+        <h1>
+            Title
+        </h1>
+        <h3>
+            Author
+        </h3>
+        <p>
+            Content
+        </p>
+    </div>
+);
+}
+export default Post;

8114ad4fb489e29a5f163a8fcdec2d771ef70ed7
Meryem Yuksel <myuksel@ufl.edu>
2022-11-14 14:41:52 -0500

    Working with useAxios to try to connect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,15 @@
 import PostList from "../components/PostList";
+import useAxios from "../utils/useAxios";
 const Home = () => {
+    const {data: blogs, isPending, error} = useAxios('http://localhost:8000/blogs');
     return (
         <div>
         <h1>Home Page</h1>
+       <div className="home">
+         /*   {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
+            <PostList blogs={blogs} title="All Blogs"/>*/
+        </div>
         </div>
     );
 }

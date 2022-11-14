eb045cec97ed1edcc09c0084331135d0d3d34cd6
Courtney Leu <courtneygleu@outlook.com>
2022-11-14 13:18:05 -0500

    cl

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,26 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+describe("Teset login", ()=>{
+  test("sign in process works", () => {
+    render(<App />);
+    expect(screen.getByRole("heading")).toHaveTextContent("Email Address");
+    expect(screen.getByRole("combobox")).toHaveDisplayValue("Enter Email");
+    expect(screen.getByRole("heading")).toHaveTextContent("Password");
+    expect(screen.getByRole("combobox")).toHaveDisplayValue("Password");
+        render(<Login/>);
+        const buttonList = await screen.findAllByRole("button");
+        expect(buttonList).toHaveLength(2);
+  });
+
+  test("test should fail login", ()=> {
+      const testEmail = "test.com";
+      expect(validateEmail(testEmail)).not.toBe(true);
+  });
+
+});
+
+
+

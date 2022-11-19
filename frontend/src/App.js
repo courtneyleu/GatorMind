cba1d0da5b3997c229b8334bcb0a3563b7b663fd
singh-maya <singh.maya@ufl.edu>
2022-11-19 16:29:11 -0500

    added firebase backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,18 +7,11 @@ import Home from "./pages/Home";
 import Default from "./pages/Default"
 import Profile from "./pages/Profile";
 import Settings from "./pages/Settings"
-<<<<<<< HEAD
-import {Bell, CircleFill, Gear, HouseDoor} from "react-bootstrap-icons";
-import Post from "./components/Post";
-import PostList from "./components/PostList";
-import Category from "./components/Category";
-import EventBus from "./common/EventBus";
-=======
 import {BackspaceReverse, Bell, CircleFill, Gear, HouseDoor} from "react-bootstrap-icons";
 import {useAuthState} from "react-firebase-hooks/auth";
 import {auth, logout} from "./services/firebase"
+import {MDBIcon} from "mdb-react-ui-kit";
 
->>>>>>> 6eec7d37a0aaff067b8dd789d03072c19f059412
 
 function App(){
   const [user, loading, error] = useAuthState(auth);
@@ -27,9 +20,10 @@ function App(){
   return (
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <Link to={"/"} className="navbar-brand">
-            GatorMind
-          </Link>
+            <a className="navbar-brand" href="#">
+                <MDBIcon fas icon="brain" />
+                     GatorMind
+            </a>
           {user &&
           <div className="navbar-nav ml-auto">
             <li className='nav-item'>
@@ -94,9 +88,6 @@ function App(){
             <Route path="/register" element={<Register />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/settings" element={<Settings />} />
-            <Route path = "/post/:id" element = {<Post/>}/>
-            <Route path = "/post" element = {<PostList/>}/>
-            <Route path = "/category/:id" element = {<Category/>}/>
           </Routes>
         </div>
       </div>

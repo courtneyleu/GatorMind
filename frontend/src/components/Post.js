fc4203419c9a91405151f9dc17ad21d6282346d7
singh-maya <87378136+singh-maya@users.noreply.github.com>
2022-11-29 18:41:56 -0500

    added comment to post
    adds comment to comment database but not to the post database

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,76 +1,179 @@
-import React, { useState, useEffect } from 'react';
-import { Link , useLocation } from 'react-router-dom';
-import axios from 'axios';
-import {collection, query, getDocs} from "firebase/firestore";
-import { db} from '../services/firebase'
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import axios from "axios";
+import { auth, db } from "../services/firebase";
+import {
+	MDBBtn,
+	MDBCard,
+	MDBCardBody,
+	MDBCol,
+	MDBContainer,
+	MDBInput,
+	MDBRow,
+	MDBTextArea,
+	MDBValidation,
+} from "mdb-react-ui-kit";
+import {
+	setDoc,
+	doc,
+	collection,
+	query,
+	where,
+	updateDoc,
+	getDocs,
+	arrayUnion,
+} from "firebase/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const Post = (props) => {
-    const [blog, setBlog] = useState({});
-    const [title, setTitle] = useState();
-    const [likes, setLikes] = useState();
-    const [body, setBody] = useState();
-    const [liked, setLiked] = useState(false);
-    const [createdOn, setCreated] = useState();
-    const location = useLocation();
-    const slug = location.pathname.substring(6);
-   console.log(slug);
-    useEffect(() => {
+	const [blog, setBlog] = useState({});
+	const [title, setTitle] = useState();
+	const [likes, setLikes] = useState();
+	const [body, setBody] = useState();
+	const [commentBody, setCommentBody] = useState();
+	const [liked, setLiked] = useState(false);
+	const [createdOn, setCreated] = useState();
+	const [user, loading, error] = useAuthState(auth);
 
-        const getPosts = async (user) => {
-            try {
-                const q = query(collection(db, "post"));
-                const doc = await getDocs(q);
-                console.log("getting docs now");
-                setBlog(doc.docs[slug]);
-                setLikes(doc.docs[slug].data().likes);
-                setTitle(doc.docs[slug].data().title);
-                setBody(doc.docs[slug].data().body);
-                setCreated(doc.docs[slug].data().created_on);
-                console.log(createdOn);
-                console.log("getting data from docs");
-            }
-            catch (err) {
-                console.error(err);
-            }
-        }
+	const location = useLocation();
+	const slug = location.pathname.substring(6);
+	const uid = user.uid;
 
-        getPosts();
-    }, []);
+	console.log(slug);
 
+	useEffect(() => {
+		const getPosts = async (user) => {
+			try {
+				const q = query(collection(db, "post"));
+				const doc = await getDocs(q);
+				console.log("getting docs now");
+				setBlog(doc.docs[slug]);
+				setLikes(doc.docs[slug].data().likes);
+				setTitle(doc.docs[slug].data().title);
+				setBody(doc.docs[slug].data().body);
+				setCreated(doc.docs[slug].data().created_on);
+				console.log(createdOn);
+				console.log("getting data from docs");
+			} catch (err) {
+				console.error(err);
+			}
+		};
 
-    const createBlog = () => {
-        return {__html: body}
-    };
+		getPosts();
+	}, []);
 
-    const postLiked = ()=>{
-        setLiked(!liked);
-        console.log(liked);
-        const btn = document.getElementById('btn');
-        if(!liked){
-        setLikes((likes)=> likes+1);
-        console.log(likes);
-        btn.style.backgroundColor = 'blue';
-        }
-        else{
-            setLikes((likes)=>likes-1);
-            btn.style.backgroundColor = 'lightgray';
-        }
-    }
-    return (
-        <div className='container mt-3'>
-            <h1 className='display-2'>{title}</h1>
-            <h2 className='text-muted mt-3'>Category:</h2>
-            <h4>{createdOn}</h4>
-            <div className='mt-5 mb-5' dangerouslySetInnerHTML={createBlog()} />
-            <div style={{display: 'flex',columnGap:60, alignItems: 'center', fontWeight: 'bold'}}>
-                            <button id = 'btn' onClick = {postLiked}> <img src = "https://cdn.pixabay.com/photo/2021/10/11/00/58/star-6699070_960_720.png"
-                            width="25" height= "25" />Likes: {likes}</button>   
-                        <div>Comments:</div>
-            </div>
-            <hr />
-            <p className='lead mb-5'><Link to='/posts' className='font-weight-bold'>Back to Posts</Link></p>
-        </div>
-    );
+	const makeComment = async () => {
+		if (!commentBody) {
+			document.getElementById("form").reset();
+			if (!commentBody) {
+				setBody("");
+			}
+		} else {
+			// add comment to database
+			let created_on = new Date().toJSON().slice(0, 10);
+			setCreated(createBlog);
+			const newComment = doc(collection(db, "comment"));
+			const data = {
+				uid: uid,
+				body: commentBody,
+				created_on: created_on,
+			};
+			await setDoc(newComment, data);
+
+			const blogDoc = doc(blog.id);
+			await updateDoc(blogDoc, {
+				comment: arrayUnion(newComment),
+			});
+		}
+	};
+
+	const createBlog = () => {
+		return { __html: body };
+	};
+
+	const postLiked = () => {
+		setLiked(!liked);
+		console.log(liked);
+		const btn = document.getElementById("btn");
+		if (!liked) {
+			setLikes((likes) => likes + 1);
+			console.log(likes);
+			btn.style.backgroundColor = "blue";
+		} else {
+			setLikes((likes) => likes - 1);
+			btn.style.backgroundColor = "lightgray";
+		}
+	};
+	return (
+		<div className="container mt-3">
+			<h1 className="display-2">{title}</h1>
+			<h2 className="text-muted mt-3">Category:</h2>
+			<h4>{createdOn}</h4>
+			<div
+				className="mt-5 mb-5"
+				dangerouslySetInnerHTML={createBlog()}
+			/>
+			<div
+				style={{
+					display: "flex",
+					columnGap: 60,
+					alignItems: "center",
+					fontWeight: "bold",
+				}}
+			>
+				<button
+					id="btn"
+					onClick={postLiked}
+				>
+					{" "}
+					<img
+						src="https://cdn.pixabay.com/photo/2021/10/11/00/58/star-6699070_960_720.png"
+						width="25"
+						height="25"
+					/>
+					Likes: {likes}
+				</button>
+				<div>Comments:</div>
+			</div>
+			<MDBCard className="bg-white my-2">
+				<MDBCardBody className="p-5 w-100 flex-column">
+					<h2 className=" mb-5">Comment</h2>
+
+					<MDBValidation
+						className="row g-3"
+						id="form"
+					>
+						<MDBTextArea
+							wrapperClass="mb-2"
+							label="Comment"
+							id="validationCustom03"
+							required
+							type="text"
+							value={commentBody}
+							onChange={(e) => setCommentBody(e.target.value)}
+						/>
+
+						<MDBBtn
+							className="w-100 mb-2"
+							size="md"
+							onClick={makeComment}
+						>
+							Comment
+						</MDBBtn>
+					</MDBValidation>
+				</MDBCardBody>
+			</MDBCard>
+			<hr />
+			<p className="lead mb-5">
+				<Link
+					to="/posts"
+					className="font-weight-bold"
+				>
+					Back to Posts
+				</Link>
+			</p>
+		</div>
+	);
 };
 
 export default Post;

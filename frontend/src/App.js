85199b11aebe231e8e1a2411a4fd6ebd612e62d3
singh-maya <singh.maya@ufl.edu>
2022-12-01 21:09:54 -0500

    took out notification page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,115 +1,124 @@
 import React, {Component, useState} from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import {Routes, Route, Link} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
-import Default from "./pages/Default"
+import Default from "./pages/Default";
 import Profile from "./pages/Profile";
-import Settings from "./pages/Settings"
-import {BackspaceReverse, Bell, CircleFill, Gear, HouseDoor} from "react-bootstrap-icons";
+import Settings from "./pages/Settings";
+import {
+	BackspaceReverse,
+	Bell,
+	CircleFill,
+	Gear,
+	HouseDoor,
+} from "react-bootstrap-icons";
 import {useAuthState} from "react-firebase-hooks/auth";
-import {auth, logout} from "./services/firebase"
+import {auth, logout} from "./services/firebase";
 import {MDBIcon} from "mdb-react-ui-kit";
 import PostList from "./components/PostList";
 import Post from "./components/Post";
-function App(){
-  const [user, loading, error] = useAuthState(auth);
-  const[search, SetSearch] = useState("");
-  const [blogs, setBlogs] = useState([]);
-  console.log(useAuthState(auth));
+function App() {
+	const [user, loading, error] = useAuthState(auth);
+	const [search, SetSearch] = useState("");
+	const [blogs, setBlogs] = useState([]);
+	console.log(useAuthState(auth));
 
-   const SearchBlog=(e)=>{
-        e.preventDefault();
-        setBlogs(blogs.filter((blogs)=>
-            blogs.data().title.toLowerCase().includes(search.toLowerCase())||
-            blogs.data().body.toLowerCase().includes(search.toLowerCase())
-        ));
-    };
-  return (
-      <div>
-        <nav className="navbar navbar-expand navbar-dark bg-dark">
-            <a className="navbar-brand" href="#">
-                <MDBIcon fas icon="brain" />
-                     GatorMind
-            </a>
-          {user &&
-          <div className="navbar-nav ml-auto">
-              <li className='nav-item'>
-              <form onSubmit={(e)=>{SearchBlog()}}>
-                  <input onChange={(e)=>{SetSearch(e.target.value)}}/>
-                  <button type="submit">Search</button>
-              </form>
-            </li>
-            <li className='nav-item'>
-              <button type="button" className="btn btn-dark">
-                <Link className='nav-link' to='/home'>
-                  <HouseDoor/>
-                </Link>
-              </button>
-            </li>
-            <li className='nav-item'>
-              <button type="button" className="btn btn-dark">
-                <Link className='nav-link' to='/settings'>
-                  <Gear/>
-                </Link>
-              </button>
-            </li>
-            <li className='nav-item'>
-              <button type="button" className="btn btn-dark">
-                <Link className='nav-link' to='/notifications'>
-                  <Bell/>
-                </Link>
-              </button>
-            </li>
-            <li className='nav-item'>
-              <button type="button" className="btn btn-dark">
-                <Link className='nav-link' to='/profile'>
-                  <CircleFill/>
-                </Link>
-              </button>
-            </li>
-            <li className="nav-item">
-              <button type="button" className="btn btn-dark" onClick={logout}>
-                  <Link className = 'nav-link' to='/login'>
-                      <BackspaceReverse />
-                  </Link>
-              </button>
-            </li>
-          </div>
-          }
-          {!user &&
-            <div className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link to={"/login"} className="nav-link">
-                  Login
-                </Link>
-              </li>
+	const SearchBlog = (e) => {
+		e.preventDefault();
+		setBlogs(
+			blogs.filter(
+				(blogs) =>
+					blogs.data().title.toLowerCase().includes(search.toLowerCase()) ||
+					blogs.data().body.toLowerCase().includes(search.toLowerCase())
+			)
+		);
+	};
+	return (
+		<div>
+			<nav className="navbar navbar-expand navbar-dark bg-dark">
+				<a className="navbar-brand" href="#">
+					<MDBIcon fas icon="brain" />
+					GatorMind
+				</a>
+				{user && (
+					<div className="navbar-nav ml-auto">
+						<li className="nav-item">
+							<form
+								onSubmit={(e) => {
+									SearchBlog();
+								}}
+							>
+								<input
+									onChange={(e) => {
+										SetSearch(e.target.value);
+									}}
+								/>
+								<button type="submit">Search</button>
+							</form>
+						</li>
+						<li className="nav-item">
+							<button type="button" className="btn btn-dark">
+								<Link className="nav-link" to="/home">
+									<HouseDoor />
+								</Link>
+							</button>
+						</li>
+						<li className="nav-item">
+							<button type="button" className="btn btn-dark">
+								<Link className="nav-link" to="/settings">
+									<Gear />
+								</Link>
+							</button>
+						</li>
+						<li className="nav-item">
+							<button type="button" className="btn btn-dark">
+								<Link className="nav-link" to="/profile">
+									<CircleFill />
+								</Link>
+							</button>
+						</li>
+						<li className="nav-item">
+							<button type="button" className="btn btn-dark" onClick={logout}>
+								<Link className="nav-link" to="/login">
+									<BackspaceReverse />
+								</Link>
+							</button>
+						</li>
+					</div>
+				)}
+				{!user && (
+					<div className="navbar-nav ml-auto">
+						<li className="nav-item">
+							<Link to={"/login"} className="nav-link">
+								Login
+							</Link>
+						</li>
 
-              <li className="nav-item">
-                <Link to={"/register"} className="nav-link">
-                  Sign Up
-                </Link>
-              </li>
-            </div>
-          }
-        </nav>
+						<li className="nav-item">
+							<Link to={"/register"} className="nav-link">
+								Sign Up
+							</Link>
+						</li>
+					</div>
+				)}
+			</nav>
 
-        <div className="container mt-3">
-          <Routes>
-            <Route path="/" element={<Default />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path = "/posts" element = {<PostList/>}/>
-            <Route path = "/post/:id" element = {<Post/>}/>
-          </Routes>
-        </div>
-      </div>
-    );
+			<div className="container mt-3">
+				<Routes>
+					<Route path="/" element={<Default />} />
+					<Route path="/home" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Register />} />
+					<Route path="/profile" element={<Profile />} />
+					<Route path="/settings" element={<Settings />} />
+					<Route path="/posts" element={<PostList />} />
+					<Route path="/post/:id" element={<Post />} />
+				</Routes>
+			</div>
+		</div>
+	);
 }
 
 export default App;
-

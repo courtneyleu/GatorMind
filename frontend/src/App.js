0898d3507cc0c549f348cbe3a94f4659e5b52025
courtneyleu <80422527+courtneyleu@users.noreply.github.com>
2022-12-05 14:21:07 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
-import React, {Component, useState, useEffect} from "react";
-import {Routes, Route, Link, useNavigate} from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+
+import React, { Component, useState, useEffect } from "react";
+// import {Routes, Route, Link, useNavigate} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -8,123 +10,117 @@ import Default from "./pages/Default";
 import Profile from "./pages/Profile";
 import Settings from "./pages/Settings";
 import {
-	BackspaceReverse,
-	Bell,
-	CircleFill,
-	Gear,
-	HouseDoor,
+  BackspaceReverse,
+  Bell,
+  CircleFill,
+  Gear,
+  HouseDoor,
 } from "react-bootstrap-icons";
-import {useAuthState} from "react-firebase-hooks/auth";
-import {auth, Logout} from "./services/firebase";
-import {MDBIcon} from "mdb-react-ui-kit";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, logout } from "./services/firebase";
+import { MDBIcon } from "mdb-react-ui-kit";
 import PostList from "./components/PostList";
 import Post from "./components/Post";
-import {signOut} from "firebase/auth";
-
 function App() {
-	const [user, loading, error] = useAuthState(auth);
-	const [search, SetSearch] = useState("");
-	const [blogs, setBlogs] = useState([]);
-	const navigate = useNavigate();
+  const [user, loading, error] = useAuthState(auth);
+  const [search, SetSearch] = useState("");
+  const [blogs, setBlogs] = useState([]);
+  //const navigate = useNavigate();
+  console.log(useAuthState(auth));
 
-	console.log(useAuthState(auth));
+  const SearchBlog = (e) => {
+    e.preventDefault();
+    setBlogs(
+      blogs.filter(
+        (blogs) =>
+          blogs.data().title.toLowerCase().includes(search.toLowerCase()) ||
+          blogs.data().body.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+  };
 
-	const SearchBlog = (e) => {
-		e.preventDefault();
-		setBlogs(
-			blogs.filter(
-				(blogs) =>
-					blogs.data().title.toLowerCase().includes(search.toLowerCase()) ||
-					blogs.data().body.toLowerCase().includes(search.toLowerCase())
-			)
-		);
-	};
+  useEffect(() => {
+    if (loading) {
+      return;
+    }
+    // if (user) {
+    // 	navigate("/home");
+    // } else if (user == null) {
+    // 	navigate("/login");
+    // }
+  }, [user, loading]);
 
-	const logout = () => {
-		signOut(auth);
-		console.log(auth);
-	};
+  return (
+    <Router>
+      <div>
+        {user && (
+          <nav className="navbar navbar-expand navbar-dark bg-dark">
+            <a className="navbar-brand" href="#">
+              <MDBIcon fas icon="brain" />
+              GatorMind
+            </a>
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <form
+                  onSubmit={(e) => {
+                    SearchBlog();
+                  }}
+                >
+                  <input
+                    onChange={(e) => {
+                      SetSearch(e.target.value);
+                    }}
+                  />
+                  <button type="submit">Search</button>
+                </form>
+              </li>
+              <li className="nav-item">
+                <button type="button" className="btn btn-dark">
+                  <Link className="nav-link" to="/home">
+                    <HouseDoor />
+                  </Link>
+                </button>
+              </li>
+              <li className="nav-item">
+                <button type="button" className="btn btn-dark">
+                  <Link className="nav-link" to="/settings">
+                    <Gear />
+                  </Link>
+                </button>
+              </li>
+              <li className="nav-item">
+                <button type="button" className="btn btn-dark">
+                  <Link className="nav-link" to="/profile">
+                    <CircleFill />
+                  </Link>
+                </button>
+              </li>
+              <li className="nav-item">
+                <button type="button" className="btn btn-dark" onClick={logout}>
+                  <Link className="nav-link" to="/login">
+                    <BackspaceReverse />
+                  </Link>
+                </button>
+              </li>
+            </div>
+          </nav>
+        )}
 
-	useEffect(() => {
-		if (loading) {
-			return;
-		}
-		if (user) {
-			navigate("/home");
-		} else if (user == null) {
-			return;
-		}
-	}, [user, loading]);
-
-	return (
-		<div>
-			{user && (
-				<nav className="navbar navbar-expand navbar-dark bg-dark">
-					<a className="navbar-brand" href="#">
-						<MDBIcon fas icon="brain" />
-						GatorMind
-					</a>
-					<div className="navbar-nav ml-auto">
-						<li className="nav-item">
-							<form
-								onSubmit={(e) => {
-									SearchBlog();
-								}}
-							>
-								<input
-									onChange={(e) => {
-										SetSearch(e.target.value);
-									}}
-								/>
-								<button type="submit">Search</button>
-							</form>
-						</li>
-						<li className="nav-item">
-							<button type="button" className="btn btn-dark">
-								<Link className="nav-link" to="/home">
-									<HouseDoor />
-								</Link>
-							</button>
-						</li>
-						<li className="nav-item">
-							<button type="button" className="btn btn-dark">
-								<Link className="nav-link" to="/settings">
-									<Gear />
-								</Link>
-							</button>
-						</li>
-						<li className="nav-item">
-							<button type="button" className="btn btn-dark">
-								<Link className="nav-link" to="/profile">
-									<CircleFill />
-								</Link>
-							</button>
-						</li>
-						<li className="nav-item">
-							<button type="button" className="btn btn-dark" onClick={logout}>
-								<Link className="nav-link" to="/">
-									<BackspaceReverse />
-								</Link>
-							</button>
-						</li>
-					</div>
-				</nav>
-			)}
-
-			<div className="container mt-3">
-				<Routes>
-					<Route path="/" element={<Default />} />
-					<Route path="/home" element={<Home />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/register" element={<Register />} />
-					<Route path="/profile" element={<Profile />} />
-					<Route path="/settings" element={<Settings />} />
-					<Route path="/posts" element={<PostList />} />
-					<Route path="/post/:id" element={<Post />} />
-				</Routes>
-			</div>
-		</div>
-	);
+        <div className="container mt-3">
+          <Routes>
+            <Route path="/" element={<Default />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/posts" element={<PostList />} />
+            <Route path="/post/:id" element={<Post />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;

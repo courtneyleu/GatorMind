dc51228dff8871e992009cb7bb5f53f56ca5ccf4
courtneyleu <80422527+courtneyleu@users.noreply.github.com>
2022-12-06 08:43:11 -0500

    Update App.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -11,19 +11,19 @@ it("renders without crashing", () => {
   render(<App />, div);
 });
 
-// test("renders the landings page", () => {
-//   render(
-//     <Router>
-//       <App />
-//     </Router>
-//   );
+test("renders the landings page", () => {
+render(
+<Router>
+<App />
+</Router>
+);
 
-//   <form aria-labelledby="form-label-element-id" aria-hidden />;
+<form aria-labelledby="form-label-element-id" aria-hidden />;
 
-//   expect(screen.getByRole("heading")).toHaveTextContent("Gator Mind");
-//   expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent(
-//     "A safe space to share"
-//   );
-//   expect(screen.getByRole("button", { name: "Login" })).toBeDisabled();
-//   expect(screen.getByRole("button", { name: "Register" })).toBeDisabled();
-// });
+expect(screen.getByRole("heading")).toHaveTextContent("Gator Mind");
+expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent(
+"A safe space to share"
+);
+expect(screen.getByRole("button", { name: "Login" })).toBeDisabled();
+expect(screen.getByRole("button", { name: "Register" })).toBeDisabled();
+});

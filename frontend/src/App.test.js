b3bc660e2ad94921e5635ffa4a69c56f8c5102b3
courtneyleu <80422527+courtneyleu@users.noreply.github.com>
2022-12-06 08:52:22 -0500

    Update App.test.js
    deleted extra imports

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,6 @@
 import React from "react";
-// import ReactDOM from "react-dom";
-import { Routes, Route, Link } from "react-router-dom";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
-import renderer from "react-test-renderer";
 import { Router } from "express";
 
 it("renders without crashing", () => {
@@ -12,18 +9,18 @@ it("renders without crashing", () => {
 });
 
 test("renders the landings page", () => {
-render(
-<Router>
-<App />
-</Router>
-);
+  render(
+    <Router>
+      <App />
+    </Router>
+  );
 
-<form aria-labelledby="form-label-element-id" aria-hidden />;
+  <form aria-labelledby="form-label-element-id" aria-hidden />;
 
-expect(screen.getByRole("heading")).toHaveTextContent("Gator Mind");
-expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent(
-"A safe space to share"
-);
-expect(screen.getByRole("button", { name: "Login" })).toBeDisabled();
-expect(screen.getByRole("button", { name: "Register" })).toBeDisabled();
+  expect(screen.getByRole("heading")).toHaveTextContent("Gator Mind");
+  expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent(
+    "A safe space to share"
+  );
+  expect(screen.getByRole("button", { name: "Login" })).toBeDisabled();
+  expect(screen.getByRole("button", { name: "Register" })).toBeDisabled();
 });
